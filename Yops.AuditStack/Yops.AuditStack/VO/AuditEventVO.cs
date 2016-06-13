5bd95beed68ecf3fc6d3104a472be16fe5fb5251
ygorops <ygorsantos92@gmail.com>
2016-06-13 02:05:02 -0300

    Create a AuditEventVO class

---
+++ Yops.AuditStack/Yops.AuditStack/VO/AuditEventVO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yops.AuditStack.VO
+{
+	public class AuditEventVO
+	{
+		public string Event { get; set; }
+		public object Data { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
