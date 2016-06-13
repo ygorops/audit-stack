9fd029f9cd9490295bad147e8742e57b69f76779
ygorops <ygorsantos92@gmail.com>
2016-06-13 02:02:45 -0300

    Create a AuditVO class

---
+++ Yops.AuditStack/Yops.AuditStack/VO/AuditVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yops.AuditStack.VO
+{
+	public class AuditVO
+	{
+		public string Id { get; set; }
+		public string Author { get; set; }
+		public string Operation { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
