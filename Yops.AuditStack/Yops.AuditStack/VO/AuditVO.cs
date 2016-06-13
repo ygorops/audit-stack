461dd6bec7139eddef6332c70b9a73d706307f59
ygorops <ygorsantos92@gmail.com>
2016-06-13 02:39:55 -0300

    Implement validation in AuditVOValidation class

--- Yops.AuditStack/Yops.AuditStack/VO/AuditVO.cs
+++ Yops.AuditStack/Yops.AuditStack/VO/AuditVO.cs
@@ -7,7 +7,7 @@
 		public string Id { get; set; }
 		public string Author { get; set; }
 		public string Operation { get; set; }
-		public DateTime Date { get; set; }
+		public DateTime? Date { get; set; }
 		public AuditEventVOCollection Events { get; set; }
 	}
 }
--- Yops.AuditStack/Yops.AuditStack/Validation/AuditVOValidation.cs
+++ Yops.AuditStack/Yops.AuditStack/Validation/AuditVOValidation.cs
@@ -1,7 +1,29 @@
-﻿namespace Yops.AuditStack.Validation
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Yops.AuditStack.VO;
+
+namespace Yops.AuditStack.Validation
 {
 	internal class AuditVOValidation
 	{
 		internal AuditVOValidation() { }
+
+		internal void Validate(AuditVO auditVO)
+		{
+			List<Exception> exceptions = new List<Exception>();
+
+			if (string.IsNullOrWhiteSpace(auditVO.Author))
+				exceptions.Add(new ArgumentNullException("AuditVO.Author"));
+			if (!auditVO.Date.HasValue)
+				exceptions.Add(new ArgumentNullException("AuditVO.Date"));
+			if (string.IsNullOrWhiteSpace(auditVO.Operation))
+				exceptions.Add(new ArgumentNullException("AuditVO.Operation"));
+			if (string.IsNullOrWhiteSpace(auditVO.Id))
+				exceptions.Add(new ArgumentNullException("AuditVO.Id"));
+
+			if (exceptions.Any())
+				throw new AggregateException(exceptions);
+		}
 	}
 }
