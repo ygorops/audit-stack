5db16da2c95314b43e57eb72248cdf7c72fc6f0b
ygorops <ygorsantos92@gmail.com>
2016-06-18 19:05:47 -0300

    Separate Dog class to own file

--- Yops.AuditStack/Yops.AuditStack.Test/AuditTest.cs
+++ Yops.AuditStack/Yops.AuditStack.Test/AuditTest.cs
@@ -12,7 +12,6 @@
 	public class AuditTest
 	{
 		private static List<AuditVO> _audits = new List<AuditVO>();
-		private const string AUTHOR_VALUE = "testAuthor";
 
 		[OneTimeSetUp]
 		public void SetUp()
@@ -124,65 +123,18 @@
 			Assert.AreEqual(auditList[1].Author, author);
 		}
 
-		public class Dog : Audit
-		{
-			public string Name { get; set; }
-			public string Owner { get; set; }
+        [Test]
+        [Order(6)]
+        public void GetAuditByOperation()
+        {
+            // Arrange
 
-			public void Save()
-			{
-				// Validate Owner
-				ValidateOwner();
-				this.AuditAddEvent("checkOwner", this.Owner, DateTime.Now);
 
-				// Save in database
-				SaveSQL();
-				this.AuditAddEvent("save", this, DateTime.Now);
+            // Act
 
-				// Audit	
-				SaveAudit("saveOperation");
-			}
-
-			public async Task SaveAsync(CancellationToken cancellationToken)
-			{
-				// Validate Owner
-				ValidateOwner();
-				this.AuditAddEvent("checkOwner", this.Owner, DateTime.Now);
-
-				// Save in database
-				SaveSQL();
-				this.AuditAddEvent("save", this, DateTime.Now);
-
-				// Audit	
-				await SaveAuditAsync("saveOperation", CancellationToken.None);
-			}
-
-			#region SaveAudit
-			private void SaveAudit(string operation)
-			{
-				SetAuditParameters(operation);
-				this.AuditSave();
-			}
-
-			private async Task SaveAuditAsync(string operation, CancellationToken cancellationToken)
-			{
-				SetAuditParameters(operation);
-				await this.AuditSaveAsync(cancellationToken);
-			}
-
-			private void SetAuditParameters(string operation)
-			{
-				this.AuditSetId(Guid.NewGuid().ToString());
-				this.AuditSetAuthor(AUTHOR_VALUE);
-				this.AuditSetOperation(operation);
-				this.AuditSetDate(DateTime.Now);
-			}
-			#endregion
-
-			private void SaveSQL() { }
-			private void ValidateOwner() { }
-		}
-
+            // Assert
+        }
+        
 		internal class AuditPersistenceTest : IAuditStackPersistense
 		{
 			public AuditVO Get(string id)
---
+++ Yops.AuditStack/Yops.AuditStack.Test/Dog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Yops.AuditStack.Test
+{
+    public class Dog : Audit
+    {
+        private const string AUTHOR_VALUE = "testAuthor";
+
+        public string Name { get; set; }
+        public string Owner { get; set; }
+
+        public void Save()
+        {
+            // Validate Owner
+            ValidateOwner();
+            this.AuditAddEvent("checkOwner", this.Owner, DateTime.Now);
+
+            // Save in database
+            SaveSQL();
+            this.AuditAddEvent("save", this, DateTime.Now);
+
+            // Audit	
+            SaveAudit("saveOperation");
+        }
+
+        public async Task SaveAsync(CancellationToken cancellationToken)
+        {
+            // Validate Owner
+            ValidateOwner();
+            this.AuditAddEvent("checkOwner", this.Owner, DateTime.Now);
+
+            // Save in database
+            SaveSQL();
+            this.AuditAddEvent("save", this, DateTime.Now);
+
+            // Audit	
+            await SaveAuditAsync("saveOperation", CancellationToken.None);
+        }
+
+        #region SaveAudit
+        private void SaveAudit(string operation)
+        {
+            SetAuditParameters(operation);
+            this.AuditSave();
+        }
+
+        private async Task SaveAuditAsync(string operation, CancellationToken cancellationToken)
+        {
+            SetAuditParameters(operation);
+            await this.AuditSaveAsync(cancellationToken);
+        }
+
+        private void SetAuditParameters(string operation)
+        {
+            this.AuditSetId(Guid.NewGuid().ToString());
+            this.AuditSetAuthor(AUTHOR_VALUE);
+            this.AuditSetOperation(operation);
+            this.AuditSetDate(DateTime.Now);
+        }
+        #endregion
+
+        private void SaveSQL() { }
+        private void ValidateOwner() { }
+    }
+}
